788adcf744368c7213113a601dc76d09c4dabec3
meison <meisonrojas33@gmail.com>
2023-03-22 21:16:00 -0500

    tarea 9

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,43 +3,35 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Cliente cliente = new Cliente();
+        Trabajador trabajador = new Trabajador();
+        cliente.nombre = "Yoel";
+        cliente.edad = 42;
+        cliente.telefono = 934456213;
+        cliente.credito = 150.55;
+        System.out.println("Soy " + cliente.nombre + ", tengo " + cliente.edad + " años y mi telefono es " + cliente.telefono
+                + " y mi credito disponible es " + cliente.credito + "€");
 
-        Persona persona = new Persona();
-        persona.setNombre("Meison");
-        System.out.println("Mi nombre es " + persona.getNombre());
-        persona.setEdad(19);
-        System.out.println("Tengo " + persona.getEdad() + " años");
-        persona.setTelefono(918039661);
-        System.out.println("Mi teléfono es " + persona.getTelefono());
+        trabajador.nombre = "Gabriel";
+        trabajador.edad = 34;
+        trabajador.telefono = 994423345;
+        trabajador.salario = 3000.40;
+        System.out.println("Soy " + trabajador.nombre + ", tengo " + trabajador.edad + " años y mi telefono es " + trabajador.telefono
+                + " y mi salario es de " + trabajador.salario + "€");
+
     }
 }
 
-class Persona {
-    private String nombre;
-    private int edad;
-    private int telefono;
+class Persona{
+    int edad;
+    int telefono;
+    String nombre;
+}
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+class Cliente extends Persona{
+    double credito;
+}
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setEdad(int edad){
-        this.edad = edad;
-    }
-
-    public int getEdad() {
-        return edad;
-    }
-
-    public void setTelefono(int telefono){
-        this.telefono = telefono;
-    }
-
-    public int getTelefono() {
-        return telefono;
-    }
+class Trabajador extends Persona {
+    double salario;
 }
